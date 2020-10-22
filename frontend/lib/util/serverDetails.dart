aaa540ef1019dfcc08415518d7d06ec6af9be18d
LINLUOOO <49470178+LINLUOOO@users.noreply.github.com>
2020-10-22 13:27:51 +0800

    update server ip

--- frontend/lib/util/serverDetails.dart
+++ frontend/lib/util/serverDetails.dart
@@ -1,5 +1,5 @@
 class ServerDetails {
-  static String ip = "http://18.189.189.7";
+  static String ip = "http://3.25.215.33";
   static String port = "8080";
   static String api = "/medsec/api/";
 }
